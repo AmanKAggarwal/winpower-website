c3736779d20334b3886c43a15f74b493d9158e3d
Aman Aggarwal <akaggarw@ucsd.edu>
2024-12-25 20:06:04 -0800

    fix routing for subpages

--- src/App.tsx
+++ src/App.tsx
@@ -4,21 +4,30 @@ import Footer from "./components/Footer";
 import NotFoundPage from "./pages/NotFoundPage";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { ROUTES } from "./Routes";
+import { RouteElement } from "./types/RouteElement";
 
-const renderRoutes = (routes: typeof ROUTES) => {
-  return routes.map((route) => (
-    <Route key={route.path} path={route.path} element={route.component}>
-      {route.children && renderRoutes(route.children)}
-    </Route>
-  ));
+const getFlattenedRouteElements = (routes: RouteElement[]): RouteElement[] => {
+  const flattenedRoutes = [];
+  for (const route of routes) {
+    flattenedRoutes.push(route);
+    if (route.children) {
+      const flattened = getFlattenedRouteElements(route.children);
+      flattenedRoutes.push(...flattened);
+    }
+  }
+  return flattenedRoutes;
 };
 
 const App: React.FC = () => {
+  const routeElements = getFlattenedRouteElements(ROUTES);
+
   return (
     <Router>
       <Navbar />
       <Routes>
-        {renderRoutes(ROUTES)}
+        {routeElements.map((route) => (
+          <Route key={route.path} path={route.path} element={route.component} />
+        ))}
         {/* Catch-all route for 404 */}
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
